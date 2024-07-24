6f903cf8505792c6eb57881da647e01d6c54c042
moonyoulove <charlie851213@gmail.com>
2024-07-24 21:11:07 +0800

    surport typescript and prepare bytesRange
    wait v4.wasm can work to use bytesRange

--- extension.js
+++ extension.js
@@ -1,79 +1,91 @@
 const extensionName = "dprint";
-const vscode = require('vscode');
+const vscode = require("vscode");
 const { createFromBuffer } = require("@dprint/formatter");
 const { getPath } = require("@dprint/typescript");
 const fs = require("fs");
-const dprint = { formatter: null };
+const dprint = {};
 
 function setConfig(config = {}) {
-	try {
-		dprint.formatter.setConfig({}, config);
-	} catch (error) {
-		vscode.window.showErrorMessage("Failed to set config");
-	}
+    try {
+        dprint.formatter.setConfig({}, config);
+    } catch (error) {
+        vscode.window.showErrorMessage("Failed to set config");
+    }
 }
 
-function formatText(text) {
-	try {
-		return dprint.formatter.formatText("", text);
-	} catch (error) {
-		console.error("Failed to format text");
-		return text;
-	}
+function formatText(text, language) {
+    try {
+        const filePath = language === "typescript" ? "*.ts" : "*.js";
+        return dprint.formatter.formatText({ filePath, fileText: text });
+    } catch (error) {
+        console.error("Failed to format text");
+        return text;
+    }
 }
 
 function findRange(text) {
-	const lines = text.split(/(?<=\n)/);
-	const start = lines.findIndex(line => line.match(/\S/));
-	const end = lines.findLastIndex(line => line.match(/\S/)) + 1;
-	return [
-		lines.slice(0, start).join(""),
-		lines.slice(start, end).join(""),
-		lines.slice(end).join("")
-	];
+    const lines = text.split(/(?<=\n)/);
+    const start = lines.findIndex(line => line.match(/\S/));
+    const end = lines.findLastIndex(line => line.match(/\S/)) + 1;
+    return [
+        lines.slice(0, start).join(""),
+        lines.slice(start, end).join(""),
+        lines.slice(end).join(""),
+    ];
+}
+
+function getBytesRange(document, range) {
+    const documentStart = new vscode.Position(0, 0);
+    const start = Buffer.byteLength(document.getText(new vscode.Range(documentStart, range.start)));
+    const end = Buffer.byteLength(document.getText(range.with(documentStart)));
+    return [start, end];
 }
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	const buffer = fs.readFileSync(getPath());
-	dprint.formatter = createFromBuffer(buffer);
-	const config = vscode.workspace.getConfiguration(extensionName).get("config");
-	setConfig(config);
+    const buffer = fs.readFileSync(getPath());
+    dprint.formatter = createFromBuffer(buffer);
+    const config = vscode.workspace.getConfiguration(extensionName).get("config");
+    setConfig(config);
 
-	context.subscriptions.push(
-		vscode.languages.registerDocumentRangeFormattingEditProvider(["javascript", "typescript"], {
-			provideDocumentRangeFormattingEdits(document, range) {
-				const text = document.getText(range);
-				const indent = text.match(/[ \t]*(?=\S)/);
-				const [spaceBefore, body, spaceAfter] = findRange(text);
-				const result = formatText(body).slice(0, -1).replaceAll(/^/gm, indent);
-				return [new vscode.TextEdit(range, spaceBefore + result + spaceAfter)];
-			}
-		}),
-		vscode.languages.registerDocumentFormattingEditProvider(["javascript", "typescript"], {
-			provideDocumentFormattingEdits(document) {
-				const text = document.getText();
-				const range = new vscode.Range(document.positionAt(0), document.positionAt(text.length));
-				const result = formatText(text);
-				return [new vscode.TextEdit(range, result)];
-			}
-		}),
-		vscode.workspace.onDidChangeConfiguration(({ affectsConfiguration }) => {
-			if (affectsConfiguration("dprint.config")) {
-				const config = vscode.workspace.getConfiguration(extensionName).get("config");
-				setConfig(config);
-			}
-		})
-	);
+    context.subscriptions.push(
+        vscode.languages.registerDocumentRangeFormattingEditProvider(["javascript", "typescript"], {
+            provideDocumentRangeFormattingEdits(document, range) {
+                const text = document.getText(range);
+                const language = document.languageId;
+                const indent = text.match(/[ \t]*(?=\S)/);
+                const [spaceBefore, body, spaceAfter] = findRange(text);
+                const result = formatText(body, language).slice(0, -1).replaceAll(/^/gm, indent);
+                return [new vscode.TextEdit(range, spaceBefore + result + spaceAfter)];
+            },
+        }),
+    );
+
+    context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider(["javascript", "typescript"], {
+        provideDocumentFormattingEdits(document) {
+            const text = document.getText();
+            const language = document.languageId;
+            const range = new vscode.Range(document.positionAt(0), document.positionAt(text.length));
+            const result = formatText(text, language);
+            return [new vscode.TextEdit(range, result)];
+        },
+    }));
+
+    context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(({ affectsConfiguration }) => {
+        if (affectsConfiguration("dprint.config")) {
+            const config = vscode.workspace.getConfiguration(extensionName).get("config");
+            setConfig(config);
+        }
+    }));
 }
 
 function deactivate() {
-	dprint.formatter = null;
+    dprint.formatter = null;
 }
 
 module.exports = {
-	activate,
-	deactivate
+    activate,
+    deactivate,
 };
